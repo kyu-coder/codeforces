e2e11738dfbc959e76d5a4e022580cde93fa18a2
Kyu <kyu.fastai@gmail.com>
2020-09-29 23:58:08 -0500

    comment

--- 1400_fixing_typos.cpp
+++ 1400_fixing_typos.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main()
 {
-	// get rid of 3 consecutives -> only 1, 2 consec. left
+	
 	string s, str;
 	cin >> s;
 
 	int count = 0;
 	char lastletter = s[0];
 	
+	// get rid of 3 consecutives -> only 1, 2 consec. left
 	if(s[1] == '\0')
 	{
 		cout << s;
@@ -36,12 +37,16 @@ int main()
 		}
 	}
 
+	// no sequence is longer than 2.
+	// if string length is 3 or less, print and end program
 	if (str[1] == '\0' || str[2] == '\0' || str[3] == '\0')
 	{
 		cout << str;
 		return 0;
 	}
 
+
+	// check for 2 consec, 2 consec cases and remove optimally
 	for (int i = 2; str[i] != '\0' ; i++)
 	{
 		if (str[i-1] == str[i-2] && str[i] == str[i+1])
