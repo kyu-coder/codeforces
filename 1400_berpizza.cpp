35e0dcd105094f177c4e9054cd8a405c5e7ddb6c
Kyu <kyu.fastai@gmail.com>
2021-01-28 16:30:23 -0600

    berpizza passed!

--- 1400_berpizza.cpp
+++ 1400_berpizza.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 typedef pair<int, int> pairs;
 
-bool sortbysec(const pairs &a, 
-              const pairs &b) 
-{ 
-    return (a.second < b.second); 
-} 
+struct comp
+{
+    template<typename T>
+    bool operator()(const T& l, const T& r) const
+    {
+		
+        if (l.second == r.second)
+            return l.first < r.first;
+ 
+        return l.second > r.second;
+    }
+};
 
 int main()
 {
@@ -20,8 +27,14 @@ int main()
 
 	int query;
 	int i_th_customer = 1, money_spent;
-	
-	set<pairs> monoset, polyset;
+
+	/* monoset will automatically be sorted and the first of the pairs is
+	 * guaranteed to be unique */
+	set<pairs> monoset;
+
+	/* but polyset needs to be sorted by the second element of the pairs
+	 * - (how much customers will spend) so I used a customized comparator */
+	set<pairs, comp> polyset;
 
 	cin >> q;
 
@@ -30,49 +43,35 @@ int main()
 
 		cin >> query;
 
-		if (query == 1)
+		if (query == 1) // customer comes in
 		{
 			cin >> money_spent;
 			pairs temp = make_pair(i_th_customer++, money_spent);
 			monoset.insert(temp);
 			polyset.insert(temp);
-			sort(polyset.begin(), polyset.end(), sortbysec);
 		}
 
-		else if (query == 2)
+		else if (query == 2) // monocarp serves a customer
 		{
+			auto it = monoset.begin(); // find the first element in monoset
 			
+			cout << it -> first << " ";
+
+			pairs temp = make_pair(it -> first, it -> second);
+			monoset.erase(temp); // erase the customer from monocarp's set
+			polyset.erase(temp); // erase the customer from polycarp's set
 		}
 
-		else // if (query == 3)
+		else // if (query == 3) polycarp serves a customer
 		{
-			
+			auto it = polyset.begin();
+
+			cout << it -> first << " ";
+
+			pairs temp = make_pair(it -> first, it -> second);
+			polyset.erase(temp); // erase the customer from polycarp's set
+			monoset.erase(temp);
 		}
 	}
 
-	for (auto const &x : polyset)
-	{
-		cout << x.first << ", " << x.second << "\n";
-	}
-	for (auto const &x : monoset)
-	{
-		cout << x.first << ", " << x.second << "\n";
-	}
-
-	
 }
-/*
-11
-1 103913
-3
-1 399791
-1 297150
-3
-1 138455
-1 192143
-3
-3
-1 39878
-3
-
-*/
